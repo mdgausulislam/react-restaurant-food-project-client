c48127f04207d3b3f0c9c36feb1fdd6334d6888a
unknown <gausulislam5@gmail.com>
2023-11-10 23:44:31 +0600

    swipper added in image

---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
+
+import slide1 from "../../../assets/home/slide1.jpg"
+import slide2 from "../../../assets/home/slide2.jpg"
+import slide3 from "../../../assets/home/slide3.jpg"
+import slide4 from "../../../assets/home/slide4.jpg"
+import slide5 from "../../../assets/home/slide5.jpg"
+
+const Category = () => {
+    return (
+        <Swiper
+            slidesPerView={4}
+            spaceBetween={30}
+            centeredSlides={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper mb-24"
+
+        >
+
+            {/* <SwiperSlide>Slide 2</SwiperSlide> */}
+            <SwiperSlide>
+                <img src={slide1} alt="" />
+                <h3 className='text-4xl text-center -mt-16 font-bold text-white'>Salads</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide2} alt="" />
+                <h3 className='text-4xl text-center -mt-16 font-bold text-white'>PIZZAS</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide3} alt="" />
+                <h3 className='text-4xl text-center -mt-16 font-bold text-white'>SOUPS</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide4} alt="" />
+                <h3 className='text-4xl text-center -mt-16 font-bold text-white'>DESSERTS</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide5} alt="" />
+                <h3 className='text-4xl text-center -mt-16 font-bold text-white'>Salads</h3>
+            </SwiperSlide>
+
+        </Swiper>
+    );
+};
+
+export default Category;
