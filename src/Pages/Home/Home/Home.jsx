70d1e2f6df82411c775c607e9be3adce4a12f9a0
unknown <gausulislam5@gmail.com>
2023-11-11 10:23:27 +0600

    creat a bistro boss components and image overlay setup

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+import BistroBoss from '../BistroBoss';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
            <Category></Category>
+           <BistroBoss></BistroBoss>
         </div>
     );
 };
