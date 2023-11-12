df7054704acba14245d8b65ee106dd0f55c7ec1e
unknown <gausulislam5@gmail.com>
2023-11-12 23:59:30 +0600

    create a firebaseand use context

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,16 @@ import {
 import "./index.css";
 import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './Provider/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className='max-w-screen-xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
