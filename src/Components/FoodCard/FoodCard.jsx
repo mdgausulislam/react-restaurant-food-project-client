6ad63e3eff94343e7fc9769b7b11c5ce615cda86
unknown <gausulislam5@gmail.com>
2023-11-13 22:50:47 +0600

    updated added cart

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
-const FoodCard = ({item}) => {
+const FoodCard = ({ item }) => {
     const { name, image, recipe, price } = item;
-    const handleAddTOCart=(item)=>{
+    const { user } = useContext(AuthContext);
+    const navigate =useNavigate();
+
+    const handleAddTOCart = (item) => {
         console.log(item);
+        if (user) {
+            fetch('http://localhost:5000/carts')
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: "Your work has been saved",
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                    }
+                })
+        } else {
+            Swal.fire({
+                title: "please login to order the food?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Login Now!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                   navigate('/login')
+                }
+            });
+        }
 
     }
     return (
@@ -14,7 +49,7 @@ const FoodCard = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                <button onClick={()=>handleAddTOCart(item)} className="btn uppercase text-[#BB8506] border-b-4 border-[#BB8506] rounded-lg px-5 text-lg">add to cart</button>
+                    <button onClick={() => handleAddTOCart(item)} className="btn uppercase text-[#BB8506] border-b-4 border-[#BB8506] rounded-lg px-5 text-lg">add to cart</button>
                 </div>
             </div>
         </div>
