5f76f9fc3212d609b3e8f7784c7d0c912fd71cca
unknown <gausulislam5@gmail.com>
2023-11-14 10:46:19 +0600

    create a tan stack query and use this query

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,5 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
+import { AuthContext } from '../Provider/AuthProvider';
+import { useContext } from 'react';
 
-const useCart = (email) => {
+const useCart = () => {
+    const { user } = useContext(AuthContext);
+
+    const { refetch,  data: cart=[]} = useQuery({
+        queryKey: ['carts', user?.email],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            return response.json();
+        },
+    })
+    return [cart,refetch]
 };
 
 export default useCart;
