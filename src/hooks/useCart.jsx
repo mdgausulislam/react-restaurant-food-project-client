386d0dac0b08436749a31791e6b8a7f978d9a2e7
unknown <gausulislam5@gmail.com>
2023-11-16 22:03:09 +0600

    send token to verification client

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -4,11 +4,14 @@ import { useContext } from 'react';
 
 const useCart = () => {
     const { user } = useContext(AuthContext);
+    const token=localStorage.getItem
 
     const { refetch,  data: cart=[]} = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{headers:{
+                authorization:`bearer ${token}`
+            }})
             return response.json();
         },
     })
