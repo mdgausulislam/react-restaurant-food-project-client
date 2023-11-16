d1fb373348bc2721788de48e3be06b9b118130ec
unknown <gausulislam5@gmail.com>
2023-11-16 17:40:07 +0600

    make user admin and install jwt

--- src/Pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/Pages/DashBoard/AllUsers/AllUsers.jsx
@@ -2,17 +2,33 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { FaTrashAlt, FaUserShield } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
-    const { data: users = [] } = useQuery({
+    const { data: users = [],  refetch } = useQuery({
         queryKey: 'users',
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/users');
             return res.json();
         },
     });
-    const handleMakeAdmin=id=>{
-        
+    const handleMakeAdmin=user=>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`,{
+            method:'PATCH'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${user.name} is an admin now`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        })
     }
 
     const handleDelete = user => {
@@ -44,7 +60,7 @@ const AllUsers = () => {
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>{user.role === 'admin' ? 'admin' :
-                                    <button onClick={() => handleMakeAdmin(user._id)} className="btn btn-ghost btn-md bg-orange-600 text-white"><FaUserShield /></button>}</td>
+                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost btn-md bg-orange-600 text-white"><FaUserShield /></button>}</td>
                                 <td>
                                     <button onClick={() => handleDelete(user)} className="btn btn-ghost btn-md bg-red-600 text-white"><FaTrashAlt /></button>
                                 </td>
