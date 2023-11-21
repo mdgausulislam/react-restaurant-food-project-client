e55df185ff6e1c5577414409ff82c1c02c3b02c3
unknown <gausulislam5@gmail.com>
2023-11-21 11:30:13 +0600

    create a stripe card and useable publish key

--- src/Pages/DashBoard/Payment/Payment.jsx
+++ src/Pages/DashBoard/Payment/Payment.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import CheckOutForm from './CheckOutForm';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
+
+//TODO: provide publishable Key
+const stripePromise=loadStripe(import.meta.env.VITE_Payment_Gateway_PK)
+console.log(stripePromise);
 const Payment = () => {
     return (
         <div className='w-full'>
-           <SectionTitle
-           heading='Please Provide'
-           subHeading='Payment Method'
-           ></SectionTitle>
+            <SectionTitle
+                heading='Please Provide'
+                subHeading='Payment Method'
+            ></SectionTitle>
+
+            <Elements stripe={stripePromise}>
+                <CheckOutForm></CheckOutForm>
+            </Elements>
         </div>
     );
 };
