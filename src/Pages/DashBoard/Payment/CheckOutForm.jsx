300bf842398a8833f8c794445a9754787506bd7f
unknown <gausulislam5@gmail.com>
2023-11-21 12:18:59 +0600

    test stripe cardand cheeck information

--- src/Pages/DashBoard/Payment/CheckOutForm.jsx
+++ src/Pages/DashBoard/Payment/CheckOutForm.jsx
@@ -1,24 +1,38 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useState } from 'react';
 
 const CheckOutForm = () => {
     const stripe = useStripe();
-    const elements=useElements();
+    const elements = useElements();
+    const [cardError,setCardError]=useState();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if(!stripe || !elements){
+        if (!stripe || !elements) {
             return;
         }
 
-        const card=elements.getElement(CardElement);
-        if(card===null){
+        const card = elements.getElement(CardElement);
+        if (card === null) {
             return;
+        }
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        if (error) {
+            console.log('error', error);
+            setCardError(error.message)
+        }
+        else {
+            setCardError('')
+            console.log('paymentMethod:', paymentMethod);
         }
     }
     return (
         <>
-            <form onSubmit={handleSubmit}>
+            <form className='w-2/3 m-10' onSubmit={handleSubmit}>
                 <CardElement
                     options={{
                         style: {
@@ -35,10 +49,13 @@ const CheckOutForm = () => {
                         },
                     }}
                 />
-                <button type="submit" disabled={!stripe}>
-                    Pay
-                </button>
+                <div className='text-center'>
+                    <button className='btn btn-primary btn-sm mt-5' type="submit" disabled={!stripe}>
+                        Pay
+                    </button>
+                </div>
             </form>
+            {cardError && <p className='text-warning ml-8'>{cardError}</p>}
         </>
     );
 };
---
+++ src/Pages/DashBoard/Payment/_payment_integration_steps.js
@@ -0,0 +1,10 @@
+/***
+ * 1. install stripe and react stripe js
+ * 2. create a checkout form with card elements (card elements contains: card elements,expiration date ,cvs,zip code)
+ * 3. create a stripe account and get a publishable key pk
+ * 4. get card information
+ * 5. create a payment method
+ * 6. use card to test payments
+ */
+
+
